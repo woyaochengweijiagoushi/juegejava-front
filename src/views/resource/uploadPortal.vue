be5d804c5cdcc75f3d660b9513b6a6c3072182e1
maple <fengtangbuding@gmail.com>
2023-03-04 17:59:48 +0800

    feat(uploadportal.vue): uploadPortal

---
+++ src/views/resource/uploadPortal.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-dialog title="导入" width="500px" v-model="portalService.upload">
+    <div style="text-align: center; margin-top: 20px">
+      <el-upload class="upload-demo" drag>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+      </el-upload>
+    </div>
+  </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { useInjectPortalSerivce } from "@/hooks/portalService";
+import { ElMessage, ElMessageBox } from "element-plus";
+import Vue, { reactive } from "vue";
+
+const portalService = useInjectPortalSerivce();
+</script>
+
+<style scoped></style>
